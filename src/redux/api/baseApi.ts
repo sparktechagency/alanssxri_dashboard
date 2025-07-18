4934407d19cc185069086387763b020be5b9cf95
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-07-18 07:47:00 +0600

    created awards api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -34,6 +34,6 @@ const baseQueryWithLogoutOnError = async (
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: baseQueryWithLogoutOnError,
-    tagTypes: ['privacy'],
+    tagTypes: ['privacy', 'awards'],
     endpoints: () => ({}),
 });
---
+++ src/redux/features/awards/awardsApi.ts
@@ -0,0 +1,50 @@
+import { baseApi } from "../../api/baseApi";
+
+
+const awardsApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+
+        getAllAwards: builder.query({
+            query: () => ({
+                url: `/dashboard/award`,
+                method: 'GET',
+            }),
+            providesTags: ["awards"]
+        }),
+
+        createAwards: builder.mutation({
+            query: (data) => ({
+                url: '/dashboard/award',
+                method: 'POST',
+                body: data,
+            }),
+            invalidatesTags: ["awards"]
+        }),
+
+        updateAwards: builder.mutation({
+            query: ({ data, id }) => ({
+                url: `/dashboard/award/${id}`,
+                method: 'PUT',
+                body: data,
+            }),
+            invalidatesTags: ["awards"]
+        }),
+
+        deleteAwards: builder.mutation({
+            query: ({ data, id }) => ({
+                url: `/dashboard/award/${id}`,
+                method: 'DELETE',
+                body: data,
+            }),
+            invalidatesTags: ["awards"]
+        }),
+
+    }),
+});
+
+export const {
+    useGetAllAwardsQuery,
+    useCreateAwardsMutation,
+    useUpdateAwardsMutation,
+    useDeleteAwardsMutation,
+} = awardsApi;
