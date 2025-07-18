3e67f709f4e227f0fcadd8a830ea5f443226ee58
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-07-18 09:48:09 +0600

    edit awards

--- src/pages/about/Awards/Awards.tsx
+++ src/pages/about/Awards/Awards.tsx
@@ -2,12 +2,12 @@
 import { Input, Pagination, Table } from "antd";
 import { MdOutlineModeEdit } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import tableContentImage from '../../../assets/tableImage.png'
 import { CiSearch } from "react-icons/ci";
 import { FiPlus } from "react-icons/fi";
 import { useState } from "react";
 import AddAwardsModal from "../../../components/PagesComponents/About/Awards/AddAwardsModal";
 import EditAwardsModal from "../../../components/PagesComponents/About/Awards/EditAwardsModal";
+import { useGetAllAwardsQuery } from "../../../redux/features/awards/awardsApi";
 
 const Awards = () => {
     const [currentPage, setCurrentPage] = useState<number>(1);
@@ -15,7 +15,7 @@ const Awards = () => {
     const pageSize = 10;
     const handlePageChange = (page: number) => {
         setCurrentPage(page);
-    };
+    }
 
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const showModal = () => {
@@ -29,8 +29,11 @@ const Awards = () => {
     };
 
     const [isModalEditOpen, setIsModaEditlOpen] = useState<boolean>(false);
-    const showEditModal = () => {
+    const [selectedItem, setSelectedItem] = useState<any>(null);
+
+    const showEditModal = (data: any) => {
         setIsModaEditlOpen(true);
+        setSelectedItem(data)
     };
     const handleEditOk = () => {
         setIsModaEditlOpen(false);
@@ -39,93 +42,52 @@ const Awards = () => {
         setIsModaEditlOpen(false);
     };
 
+
+    const { data } = useGetAllAwardsQuery(undefined);
+
     interface DataType {
         id: string,
         title: string,
         description: string,
+        image: string,
     }
 
-    const articles: DataType[] = [
-        {
-            id: "#1233",
-            title: "Middle East M&A Deal of the Year 2015",
-            description: "In 2015, our firm won “Middle East M&A Deal of the Y"
-        },
-        {
-            id: "#1233",
-            title: "Finalist at Middle East Legal Awards 2019",
-            description: "The Ministry of Education and Higher Education align"
-        },
-        {
-            id: "#1233",
-            title: "Middle East M&A Deal of the Year 2015",
-            description: "Al-Ansari & Associates advises international and do..."
-        },
-        {
-            id: "#1233",
-            title: "Finalist at Middle East Legal Awards 2019",
-            description: "In 2015, our firm won “Middle East M&A Deal of the Y"
-        },
-        {
-            id: "#1233",
-            title: "Middle East M&A Deal of the Year 2015",
-            description: "The Ministry of Education and Higher Education align"
-        },
-        {
-            id: "#1233",
-            title: "Finalist at Middle East Legal Awards 2019",
-            description: "The Ministry of Education and Higher Education align"
-        },
-        {
-            id: "#1233",
-            title: "Middle East M&A Deal of the Year 2015",
-            description: "The Ministry of Education and Higher Education align"
-        },
-        {
-            id: "#1233",
-            title: "Middle East M&A Deal of the Year 2015",
-            description: "The Ministry of Education and Higher Education align"
-        },
-    ];
+
     const columns = [
         {
             title: "S No.",
-            dataIndex: "id",
-            render: (_: any, record: DataType) => <div>{record?.id}</div>,
+            render: (_: any, __: DataType, index: number) => <div>{(currentPage - 1) * pageSize + index + 1}</div>,
         },
         {
             title: "Title",
             dataIndex: "title",
-            render: (_: any, record: DataType) => <div>{record?.title}</div>,
+            render: (title: string) => <div>{title}</div>,
         },
         {
             title: "Description",
             dataIndex: "description",
-            render: (_: any, record: DataType) => <div>{record?.description}</div>,
+            render: (desc: string) => <div>{desc}</div>,
         },
         {
             title: "Image",
             dataIndex: "image",
-            render: () => <div>
-                <img src={tableContentImage} className=" w-20" alt="image" />
-            </div>,
+            render: (image: string) => (
+                <div>
+                    <img src={image} className="w-20" alt="award" />
+                </div>
+            ),
         },
         {
             title: "Action",
-            render: () => (
-                <div className="">
-                    <div className="flex items-center gap-3">
-
-                        {/* <Link to={`/people-management/edit-person`}> */}
-                        <MdOutlineModeEdit onClick={showEditModal} size={40} className="text-white bg-primaryColor rounded p-2 cursor-pointer" />
-                        {/* </Link> */}
-                        <RiDeleteBin6Line size={40} className="text-white bg-red-600 rounded p-2 cursor-pointer" />
-
-                    </div>
+            render: (record: DataType) => (
+                <div className="flex items-center gap-3">
+                    <MdOutlineModeEdit onClick={() => showEditModal(record)} size={40} className="text-white bg-primaryColor rounded p-2 cursor-pointer" />
+                    <RiDeleteBin6Line size={40} className="text-white bg-red-600 rounded p-2 cursor-pointer" />
                 </div>
             ),
         },
     ];
+
 
 
     return (
@@ -144,12 +106,12 @@ const Awards = () => {
                 <Table
                     columns={columns}
                     className="mt-5 overflow-x-scroll xl:overflow-auto bg-white rounded-lg"
-                    dataSource={articles}
+                    dataSource={data?.data}
                     pagination={false}
                     rowKey="_id"
                 />
             </div>
-            <EditAwardsModal isModalOpen={isModalEditOpen} handleOk={handleEditOk} handleCancel={handleEditCancel}></EditAwardsModal>
+            <EditAwardsModal isModalOpen={isModalEditOpen} handleOk={handleEditOk} handleCancel={handleEditCancel} award={selectedItem}></EditAwardsModal>
             <div className=" mt-8 flex flex-col md:flex-row justify-between items-center">
                 <div>
                     <p className=" text-lg text-black mb-5 md:mb-0">Showing 1-11 out of  1239</p>
