d98b0a9b351283070330a37ac84e49936fdd51ac
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-07-25 16:16:07 +0600

    changed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+ 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: 4173
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['dashboard.laeatery.com']
+  }
 })
